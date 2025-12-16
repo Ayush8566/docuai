c9053dd64c89e8f0820f6d2918623164cd458998
Ayush kumar <119615444+Ayush8566@users.noreply.github.com>
2025-12-16 17:37:25 +0530

    Delete backend/temp directory

--- backend/temp/1765364330121-864978/frontend/src/components/MonacoEditorWrapper.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Editor from '@monaco-editor/react';
-
-export default function MonacoEditorWrapper({ value }) {
-  return <Editor height="400px" defaultLanguage="markdown" value={value} options={{ readOnly: true }} />;
-};
